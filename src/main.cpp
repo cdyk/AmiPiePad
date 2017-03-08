e0aa9f3e9990f3eb66058bb1f2d8f033ba65ee9d
Christopher Dyken <cdyk@users.noreply.github.com>
2017-03-08 21:54:46 +0100

    Initial SDL event handling.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,73 @@
+#include <SDL2/SDL.h>
 #include <iostream>
+
+
+
+
+
+void tryAddControllers()
+{
+  int n = SDL_NumJoysticks();
+  for (int i = 0; i < n; i++) {
+    auto name = SDL_JoystickNameForIndex(i);
+    if (name == nullptr) name = "[unknown]";
+
+    std::cerr << '[' << i << "]  \"" << name << '"' << std::endl;
+
+  }
+}
+
+void purgeControllers(SDL_JoystickID joyId)
+{
+
+}
 
 
 int main(int argc, char* argv[])
 {
-  std::cerr << "moo" << std::endl;
+  if (SDL_Init(SDL_INIT_GAMECONTROLLER) != 0) {
+    std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
+    return 1;
+  }
 
+
+  bool quit = false;
+  while (!quit) {
+    SDL_Event e;
+    if (!SDL_WaitEvent(&e)) {
+      std::cerr << "SDL_WaitEvent failed: " << SDL_GetError() << std::endl;
+      break;
+    }
+
+    switch (e.type)
+    {
+    case SDL_QUIT:
+      quit = true;
+      break;
+
+    case SDL_CONTROLLERDEVICEADDED:
+      tryAddControllers();
+      break;
+
+    case SDL_CONTROLLERDEVICEREMAPPED:
+      purgeControllers(e.cdevice.which);
+      tryAddControllers();
+      break;
+
+    case SDL_CONTROLLERDEVICEREMOVED:
+      purgeControllers(e.cdevice.which);
+      break;
+
+    case SDL_JOYDEVICEADDED:
+    case SDL_JOYDEVICEREMOVED:
+      // Handled by SDL_CONTROLLERDEVICE...
+      break;
+
+    default:
+      std::cerr << "Unhandled event, type=" << std::hex << e.type << std::endl;
+    }
+  }
+
+  SDL_Quit();
   return 0;
 }
