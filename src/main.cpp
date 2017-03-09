141956173eb2a625bb328d430c24338ae27f3e4b
Christopher Dyken <cdyk@users.noreply.github.com>
2017-03-09 22:02:34 +0100

    Began writing config file parser, included rapidjson files in tree so that they get copied to remote.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <vector>
 
-
+#include <rapidjson.h>
+#include <document.h>
 
 
 
@@ -19,17 +26,60 @@ void tryAddControllers()
 
 void purgeControllers(SDL_JoystickID joyId)
 {
+}
 
+bool snarf(std::vector<char>& content, const std::string path)
+{
+  int fd = open(path.c_str(), O_RDONLY);
+  if (fd < 0) return false;
+
+  struct stat st;
+  if (fstat(fd, &st) == -1) return false;
+
+  content.resize(st.st_size);
+
+  if (read(fd, content.data(), st.st_size) != st.st_size) return false;
+  close(fd);
+
+  return true;
+}
+
+bool parseConfig(const std::string& path)
+{
+  std::vector<char> content;
+  if (!snarf(content, path)) {
+    std::cerr << "Failed to read " << path << std::endl;
+    return false;
+  }
+  content.push_back('\0');
+
+  rapidjson::Document d;
+  d.ParseInsitu(content.data());
+  if (d.HasParseError()) {
+    std::cerr << "Failed to parse " << path << std::endl;
+    return false;
+  }
+
+
+
+  return true;
 }
 
 
 int main(int argc, char* argv[])
 {
+
+  if (!parseConfig("../../../data/defaultconf.json")) {
+    std::cerr << "Failed to parse config file." << std::endl;
+    return 1;
+  }
+
   if (SDL_Init(SDL_INIT_GAMECONTROLLER) != 0) {
     std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
     return 1;
   }
 
+  tryAddControllers();
 
   bool quit = false;
   while (!quit) {
