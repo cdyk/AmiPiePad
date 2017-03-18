9361ec7e11f1f0f00a15a358e36cf82dad647890
Christopher Dyken <cdyk@users.noreply.github.com>
2017-03-18 21:41:31 +0100

    Removed checks in code that is handled by schema.

--- src/ParseConfig.cpp
+++ src/ParseConfig.cpp
@@ -19,19 +19,11 @@ namespace {
 
   bool parseSource(Context * context, const std::string & name, const rapidjson::Value & root)
   {
-    if (context->sources.find(name) != context->sources.end()) {
-      std::cerr << name << " is already a defined source.\n";
-      return false;
-    }
-    if (!root.IsObject()) { std::cerr << name << " is not a JSON object.\n"; return false; }
-
-
     std::unique_ptr<Source> src(new Source(name));
 
     for (auto it = root.MemberBegin(); it != root.MemberEnd(); ++it) {
 
       if (std::strcmp("device-name", it->name.GetString()) == 0) {
-        if (!it->value.IsString()) { std::cerr << "Source '" << name << "' device-name is not a string\n"; return false; }
         src->setDeviceName(it->value.GetString());
       }
     }
@@ -43,29 +35,24 @@ namespace {
 
   bool parseSink(Context* context, const std::string& name, const rapidjson::Value& root)
   {
-    if (!root.IsObject()) { std::cerr << name << " is not a JSON object.\n"; return false; }
-
-    if (context->sinks.find(name) != context->sinks.end()) { std::cerr << name << " is already a defined sink.\n"; }
-
     auto type = root.FindMember("type");
-    if (type != root.MemberEnd()) { std::cerr << name << " is missing compulsory type member.\n"; return false; }
-    if (!type->value.IsString()) { std::cerr << name << ", 'type' is not a string.\n"; }
 
     std::unique_ptr<Sink> sink(Sink::factory(type->value.GetString(), name));
     if (!sink) { std::cerr << "Failed to create sink type " << type->value.GetString() << "\n"; return false; }
 
     auto gpios = root.FindMember("gpio");
-    if(gpios == root.MemberEnd()) { std::cerr << name << " is missing compulsory gpio member.\n"; return false; }
-    if (!gpios->value.IsObject()) { std::cerr << name << ", gpio member is not of object type.\n"; return false; }
-
+    for (auto it = gpios->value.MemberBegin(); it != gpios->value.MemberEnd(); ++it) {
+      // TODO: Parse GPIO
+    }
 
     context->sinks.insert(std::make_pair(name, std::move(sink)));
-
     return true;
   }
 
-  bool parseMapping(Context* context, const std::string& name, const rapidjson::Value& root)
+  bool parseMapping(Context* context, const rapidjson::Value& root)
   {
+
+
     return true;
   }
 
@@ -118,31 +105,21 @@ bool parseConfig(Context * context, const std::string& schemaPath, const std::st
     return false;
   }
 
-
   if (!configDoc.IsObject()) { std::cerr << "config root is not an object\n"; return false; }
 
   auto sources = configDoc.FindMember("sources");
-  if (sources != configDoc.MemberEnd()) {
-    if (!sources->value.IsObject()) { std::cerr << "'sources' is not an object\n"; return false; }
-    for (auto it = sources->value.MemberBegin(); it != sources->value.MemberEnd(); ++it) {
-      if (!parseSource(context, it->name.GetString(), it->value)) return false;
-    }
+  for (auto it = sources->value.MemberBegin(); it != sources->value.MemberEnd(); ++it) {
+    if (!parseSource(context, it->name.GetString(), it->value)) return false;
   }
 
   auto sinks = configDoc.FindMember("sinks");
-  if (sinks != configDoc.MemberEnd()) {
-    if (!sinks->value.IsObject()) { std::cerr << "'sinks' is not an object\n"; return false; }
-    for (auto it = sinks->value.MemberBegin(); it != sinks->value.MemberEnd(); ++it) {
-      if (!parseSink(context, it->name.GetString(), it->value)) return false;
-    }
+  for (auto it = sinks->value.MemberBegin(); it != sinks->value.MemberEnd(); ++it) {
+    if (!parseSink(context, it->name.GetString(), it->value)) return false;
   }
 
   auto mappings = configDoc.FindMember("mappings");
-  if (mappings != configDoc.MemberEnd()) {
-    if (!mappings->value.IsObject()) { std::cerr << "'mappings' is not an object\n"; return false; }
-    for (auto it = mappings->value.MemberBegin(); it != mappings->value.MemberEnd(); ++it) {
-      if (!parseMapping(context, it->name.GetString(), it->value)) return false;
-    }
+  for (auto & v : mappings->value.GetArray()) {
+    if (!parseMapping(context, v)) return false;
   }
 
   return true;
